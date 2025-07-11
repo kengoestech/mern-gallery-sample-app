9cefb63c868908a082de08329a470de4f4c7dd32
KennedySalud <kennedysalud15@gmail.com>
2025-07-12 02:58:56 +0800

    Updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+const API_BASE_URL = "http://private-lb-fp-75f4d042e793c434.elb.us-east-1.amazonaws.com"
+
 export default defineConfig({
   plugins: [react()],
 })
