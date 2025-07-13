16adb328a22a8c27364e297b6e429b41b0efabef
KennedySalud <kennedysalud15@gmail.com>
2025-07-13 13:01:38 +0800

    Added port 3000

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,6 +139,13 @@ resource "aws_security_group" "fp_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    description = "Connect to site"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     description = "Allow all traffic"
     from_port   = 0
