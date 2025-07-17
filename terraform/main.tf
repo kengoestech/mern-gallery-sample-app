a33b7d573e10e31fadb7b6658dd2da2a369878a7
KennedySalud <kennedysalud15@gmail.com>
2025-07-17 17:44:34 +0800

    Comment policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -321,22 +321,22 @@ resource "aws_s3_bucket_public_access_block" "block" {
   depends_on = [aws_s3_bucket.s3_bucket_fp]
 }
 
-resource "aws_s3_bucket_policy" "public_read_policy" {
-  bucket = aws_s3_bucket.s3_bucket_fp.id
+# resource "aws_s3_bucket_policy" "public_read_policy" {
+#   bucket = aws_s3_bucket.s3_bucket_fp.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.s3_bucket_fp.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource  = "${aws_s3_bucket.s3_bucket_fp.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 
 
